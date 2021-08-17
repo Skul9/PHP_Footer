425c022b344c7d0b2d99da08f0e19072a1f6eee8
Shounak Kulkarni <79972023+Skul9@users.noreply.github.com>
2021-08-17 18:07:04 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,15 @@
+<footer>
+        <div class="main">
+            <h3>Thanks For Playing! </h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet quo cupiditate nihil nesciunt assumenda quam laudantium dolorem ea qui aperiam minima laborum quibusdam quidem.</p>
+            <ul class="icons">
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
+                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+            </ul>
+        </div>
+        <div class="extra">
+            <p>Copyright &copy;2021 <span>Name</span></p>
+        </div>
+</footer>
